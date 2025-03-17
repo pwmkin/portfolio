ab096df503b60f7c009f5faca6cdb9777cbc96bd
pwmkin <pwmkin.dev@gmail.com>
2025-03-17 00:15:59 -0300

    v1.1 updated social links

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -4,13 +4,13 @@ export const SocialLinks = () => {
   return (
     <div className="fixed right-8 top-1/2 -translate-y-1/2 bg-black p-4">
       <div className="flex flex-col gap-4">
-        <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
+        <a href="https://github.com/pwmkin" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
           <Github size={24} />
         </a>
         <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
           <Linkedin size={24} />
         </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
+        <a href="https://twitter.com/pwmkin_" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
           <Twitter size={24} />
         </a>
       </div>
