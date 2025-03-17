07b375bde7f371db12980e00323df0abc7f1f2df
pwmkin <pwmkin.dev@gmail.com>
2025-03-17 00:17:55 -0300

    v1.2 added portfolio

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,6 +9,14 @@ export interface Project {
 export const projects: Project[] = [
   {
     id: "1",
+    title: "porfolio",
+    techStack: ["Vite", "React", "TailwindCSS"],
+    description:
+      "My personal portfolio, in fact, the website you are looking at right now.",
+    repository: "https://github.com/pwmkin/portfolio",
+  },
+  {
+    id: "2",
     title: "wcalc",
     techStack: ["HTML", "JavaScript", "CSS"],
     description:
